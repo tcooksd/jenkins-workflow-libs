356630d0d78aa737214e83f098fda35c7123a65f
tcook <tcooksd@gmail.com>
2018-04-05 15:56:20 -0700

    test

--- vars/morpheusApp.groovy
+++ vars/morpheusApp.groovy
@@ -1,6 +1,6 @@
 import org.tadamhicks.JenkinsHttpClient
 
 def buildApp(String morpheusUrl, Map<?, ?> postBody, String bearerToken) {
-	JenkinsHttClient http = new JenkinsHttpClient()
+	JenkinsHttpClient http = new JenkinsHttpClient()
 	http.postJson(morpheusUrl, postBody, bearerToken)
 }
